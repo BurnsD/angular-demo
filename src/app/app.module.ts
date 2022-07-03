b61ef69d9d9b048893e7700099ed88630f470ad2
BurnsD <Dustinburnsactech@gmail.com>
2022-07-03 10:04:56 -0500

    added components needed for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule
