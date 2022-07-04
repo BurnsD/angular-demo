5f18f3bb897cac2cfc45f5fe946dfaaa74f60843
BurnsD <Dustinburnsactech@gmail.com>
2022-07-03 23:28:39 -0500

    employee card interpolation complete

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -6,48 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent {
-  employees = [
+  employee1 = [
     {
-      employeeId: 1234,
+      employeeId: 1001,
       employeeName: 'Dustin Burns',
       employeeSalary: 65000,
       employeeTitle: 'Developer',
       employeeImg: '../../assets/Dustin.jpg',
     },
+  ];
+
+  employee2 = [
     {
-      employeeId: 4321,
+      employeeId: 1002,
       employeeName: 'David Thomas',
       employeeSalary: 135000,
       employeeTitle: 'Lead Developer',
       employeeImg: 'assets/David.jpg',
     },
+  ];
+  employee3 = [
     {
-      employeeId: 1432,
+      employeeId: 1003,
       employeeName: 'James Gosling',
       employeeSalary: 165000,
-      EmployeeTitle: 'Lead Engineer',
+      employeeTitle: 'Lead Engineer',
       employeeImg: 'assets/James.jpg',
     },
+  ];
+  employee4 = [
     {
-      employeeId: 1342,
+      employeeId: 1004,
       employeeName: 'Linus Torvalds',
       employeeSalary: 155000,
-      employeeTitle: 'Linux Engineer',
+      employeeTitle: 'Linux Support',
       employeeImg: 'assets/Linus.jpg',
     },
+  ];
+  employee5 = [
     {
-      employeeId: 4123,
+      employeeId: 1005,
       employeeName: 'Bjarne Stroustrup',
       employeeSalary: 95000,
       employeeTitle: 'Engineer',
       employeeImg: 'assets/Bjarne.jpg',
     },
+  ];
+  employee6 = [
     {
       employeeId: 1006,
       employeeName: 'Rushabh Mehta',
       employeeSalary: 195000,
       employeeTitle: 'Director',
       employeeImg: 'assets/Rushabh.jpg',
-    }
+    },
   ];
 }
